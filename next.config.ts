733db4ba610451c65b2dbc788f2b18282563f245
Zanakan12 <zanakan12maniac@proton.me>
2025-05-12 13:33:18 +0200

    rm rules

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  rules: {
-    'react/no-unescaped-entities': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
-  }
+  rules: {}
   
 };
 
